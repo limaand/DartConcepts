fb0d8f877dd6cd7bfcd52449f4c5e4ec8fa77bfb
limaand <limaand@gmail.com>
2023-08-18 14:20:14 -0300

    Class Abstratas

--- meu_app_oo/bin/meu_app_oo.dart
+++ meu_app_oo/bin/meu_app_oo.dart
@@ -1,9 +1,7 @@
-//import 'package:meu_app_oo/meu_app_oo.dart' as meu_app_oo;
-
-import 'package:meu_app_oo/classes/Pessoa.dart';
+import 'package:meu_app_oo/classes/PessoaFisica.dart';
 
 void main(List<String> arguments) {
-  var p1 = new Pessoa("Karla", "Loteamento Durville Quadra P");
+  var p1 = new PessoaFisica("Karla", "Loteamento Durville Quadra P", "0064646");
 
   print(p1.toString());
 }
